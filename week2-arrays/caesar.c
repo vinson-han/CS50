0c41e4b7ded3e263d7a49929e9d574ea8fd999a1
vinson-han <44034109+vinson-han@users.noreply.github.com>
2020-04-30 05:49:51 -0700

    Update caesar.c

--- week2-arrays/caesar.c
+++ week2-arrays/caesar.c
@@ -7,45 +7,55 @@
 
 
 
-int main(int argc, string argv[]){
-
+int main(int argc, string argv[])
+{
     int key;
     string s;
-    char letterA = 97;
-    
-    if(argc != 2){
+    //Check if arguement count is inputed correctly or return error statement
+    if (argc != 2)
+    {
         printf("Usage: ./caesar key\n");
         return 1;
-    } else{
-        for(int i=0, n=strlen(argv[1]); i< n; ++i){
-            if(!isdigit(argv[1][i])){
+    }
+    else //Since string is an array of character check invidual character if noit digit return error
+    {  
+        for (int i = 0, n = strlen(argv[1]); i < n; ++i)
+        {
+            if (!isdigit(argv[1][i]))
+            {
                 printf("Usage: ./caesar key\n");
                 return 1;
             }
         }
-        key = atoi(argv[1]);
+        key = atoi(argv[1]); // convert argv[1] from string to int
     }
     
      
- 
-    s= get_string("Plain Text: ");
-    
-    for(int i =0, n=strlen(s); i<n;++i){
-
-        if((s[i] > 96 && s[i] < 123) || (s[i] > 40 && s[i] <  91 )){
-            
-            if(isupper(s[i])){
-                    int cypher = (s[i] - 65 + key) % 26;
-                    printf("%c",cypher+65);
-                    
-            } else {
-
-                int cypher = (s[i] - 97 + key) % 26;
-                printf("%c",cypher+97);
+    //get user input
+    s = get_string("Plain Text: ");
+    //loop through string
+    for (int i = 0, n = strlen(s); i < n; ++i) //check if ascia value of a-z ot A-Z
+    {  
+        if ((s[i] > 96 && s[i] < 123) || (s[i] > 40 && s[i] <  91))
+        {
+            //if uppercase use proper asic values for Casear Cipher Formula
+            if (isupper(s[i]))
+            {
+                int cypher = (s[i] - 65 + key) % 26;
+                printf("%c", cypher + 65);
             }
-        }else
-            printf("%c",s[i]);
+            else
+            {
+                int cypher = (s[i] - 97 + key) % 26;
+                printf("%c", cypher + 97);
+            }
+        }// print chatracter if not alphabet
+        else
+        {
+            printf("%c", s[i]);
+        }
     }
+
     printf("\n");
     return 0;
 }
