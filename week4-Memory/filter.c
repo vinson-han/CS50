4e1ac62017500c85e32490f34eb74e099b087bdd
vinson-han <44034109+vinson-han@users.noreply.github.com>
2020-05-14 06:00:27 -0700

    Update filter.c

--- week4-Memory/filter.c
+++ week4-Memory/filter.c
@@ -1,163 +1,153 @@
+#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #include "helpers.h"
-#include <stdio.h>
-#include <math.h>
 
-// Convert image to grayscale
-void grayscale(int height, int width, RGBTRIPLE image[height][width])
+int main(int argc, char *argv[])
 {
-   
-    for (int i = 0,h = height; i < h; i++)
+
+    // Define allowable filters
+    char *filters = "begr";
+
+    // Get filter flag and check validity
+    char filter = getopt(argc, argv, filters);
+    if (filter == '?')
     {
-        for (int j = 0, w = width; j < width; j++)
+        fprintf(stderr, "Invalid filter.\n");
+        return 1;
+    }
+
+    // Ensure only one filter
+    if (getopt(argc, argv, filters) != -1)
+    {
+        fprintf(stderr, "Only one filter allowed.\n");
+        return 2;
+    }
+
+    // Ensure proper usage
+    if (argc != optind + 2)
+    {
+        fprintf(stderr, "Usage: filter [flag] infile outfile\n");
+        return 3;
+    }
+
+    // Remember filenames
+    char *infile = argv[optind];
+    char *outfile = argv[optind + 1];
+
+    // Open input file
+    FILE *inptr = fopen(infile, "r");
+    if (inptr == NULL)
+    {
+        fprintf(stderr, "Could not open %s.\n", infile);
+        return 4;
+    }
+
+    // Open output file
+    FILE *outptr = fopen(outfile, "w");
+    if (outptr == NULL)
+    {
+        fclose(inptr);
+        fprintf(stderr, "Could not create %s.\n", outfile);
+        return 5;
+    }
+
+    // Read infile's BITMAPFILEHEADER
+    BITMAPFILEHEADER bf;
+    fread(&bf, sizeof(BITMAPFILEHEADER), 1, inptr);
+
+    // Read infile's BITMAPINFOHEADER
+    BITMAPINFOHEADER bi;
+    fread(&bi, sizeof(BITMAPINFOHEADER), 1, inptr);
+
+    // Ensure infile is (likely) a 24-bit uncompressed BMP 4.0
+    if (bf.bfType != 0x4d42 || bf.bfOffBits != 54 || bi.biSize != 40 ||
+        bi.biBitCount != 24 || bi.biCompression != 0)
+    {
+        fclose(outptr);
+        fclose(inptr);
+        fprintf(stderr, "Unsupported file format.\n");
+        return 6;
+    }
+
+    int height = abs(bi.biHeight);
+    int width = bi.biWidth;
+
+    // Allocate memory for image
+    RGBTRIPLE(*image)[width] = calloc(height, width * sizeof(RGBTRIPLE));
+    if (image == NULL)
+    {
+        fprintf(stderr, "Not enough memory to store image.\n");
+        fclose(outptr);
+        fclose(inptr);
+        return 7;
+    }
+
+    // Determine padding for scanlines
+    int padding = (4 - (width * sizeof(RGBTRIPLE)) % 4) % 4;
+
+    // Iterate over infile's scanlines
+    for (int i = 0; i < height; i++)
+    {
+        // Read row into pixel array
+        fread(image[i], sizeof(RGBTRIPLE), width, inptr);
+
+        // Skip over padding
+        fseek(inptr, padding, SEEK_CUR);
+    }
+
+    // Filter image
+    switch (filter)
+    {
+        // Blur
+        case 'b':
+            blur(height, width, image);
+            break;
+
+        // Edges
+        case 'e':
+            edges(height, width, image);
+            break;
+
+        // Grayscale
+        case 'g':
+            grayscale(height, width, image);
+            break;
+
+        // Reflect
+        case 'r':
+            reflect(height, width, image);
+            break;
+    }
+
+    // Write outfile's BITMAPFILEHEADER
+    fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, outptr);
+
+    // Write outfile's BITMAPINFOHEADER
+    fwrite(&bi, sizeof(BITMAPINFOHEADER), 1, outptr);
+
+    // Write new pixels to outfile
+    for (int i = 0; i < height; i++)
+    {
+        // Write row to outfile
+        fwrite(image[i], sizeof(RGBTRIPLE), width, outptr);
+
+        // Write padding at end of row
+        for (int k = 0; k < padding; k++)
         {
-            double total = 0;
-            total += image[i][j].rgbtBlue;
-            total += image[i][j].rgbtRed;
-            total += image[i][j].rgbtGreen;
-
-            int average = round(total/3);
-            //printf("%i\n",average);
-            image[i][j].rgbtBlue = average;
-            image[i][j].rgbtRed = average;
-            image[i][j].rgbtGreen = average;
+            fputc(0x00, outptr);
         }
-    }  
-    return;
-}
+    }
 
-// Reflect image horizontally
-void reflect(int height, int width, RGBTRIPLE image[height][width])
-{
-   
-    //RGBTRIPLE temp = image[height-1][width-1];
-    // printf("%iheight\n",height);
-    // printf("%iwidth/2\n",width/2);
-    // printf("%iheight\n",temp.rgbtBlue);
-    printf("%iheight\n",image[0][0].rgbtBlue);
-    printf("%iheight\n",image[0][0].rgbtGreen);
-    printf("%iheight\n",image[0][0].rgbtRed);
-    for (int i = 0,h = height; i < h; i++)
-    {
-        for (int j = 0, w = width; j <= width/2; j++)
-        {
-            RGBTRIPLE temp = image [i][j];
-            image[i][j] = image[i][w-1-j];
-            image[i][w-1-j] = temp; 
-        }
-    }  
-    return;
-}
+    // Free memory for image
+    free(image);
 
-// Blur image
-void blur(int height, int width, RGBTRIPLE image[height][width])
-{
-    for (int i = 0,h = height; i < h; i++)
-    {
-        for (int j = 0, w = width; j < w; j++)
-        {   
-            int count = 0;
-            double totalR = 0;
-            double totalB = 0;
-            double totalG = 0;
-            
-            if(i-1 >= 0 && i+1 < h && j-1 >= 0 && j+1 < w)
-            {
-               
-                for(int x = i -1; x <= i+1; x++)
-                {
-                    for(int y = j-1; y <= j+1; y++)
-                    {
-                        totalR += image[x][y].rgbtRed;
-                        totalB += image[x][y].rgbtBlue;
-                        totalG += image[x][y].rgbtGreen;
-                        count++;
-                    }
-                }
-                image[i][j].rgbtBlue = round(totalB/count);
-                image[i][j].rgbtRed =  round(totalR/count);
-                image[i][j].rgbtGreen = round(totalG/count);
-            }
-            else
-            {
-              for(int x = i -1; x <= i+1; x++)
-                {
-                    for(int y = j-1; y <= j+1; y++)
-                    { 
-                      if ((x>=0 && x<=h-1) && (y>=0 && y<= w-1))
-                      {
-                            totalR += image[x][y].rgbtRed;
-                            totalB += image[x][y].rgbtBlue;
-                            totalG += image[x][y].rgbtGreen;
-                            count++;
-                      }         
-                    }
+    // Close infile
+    fclose(inptr);
 
-                    if(count> 0)
-                    {
-                        image[i][j].rgbtBlue = round(totalB/count);
-                        image[i][j].rgbtRed =  round(totalR/count);
-                        image[i][j].rgbtGreen = round(totalG/count);
-                    }
-                }
-               
-            }
-            
-        }
-    }  
-}
+    // Close outfile
+    fclose(outptr);
 
-// Detect edges
-void edges(int height, int width, RGBTRIPLE image[height][width])
-{
-    //sqrt(gx^2 + gx^2)
-    int aGx[3][3] = {{-1,0,1},{-2,0,2},{-1,0,1}};
-    int aGy[3][3] = {{-1,-2,-1},{0,0,0},{1,2,1}};
-    //printf("%i",aGx[0][0]);
-    for (int i = 0,h = 5; i < h; i++)
-    {
-        for (int j = 0, w = 5; j < w; j++)
-        {   
-            int count = 0;
-            double gxR = 0;
-            double gxB = 0;
-            double gxG = 0;
-            double gyR = 0;
-            double gyB = 0;
-            double gyG = 0;
-          
-            
-            if(i-1 >= 0 && i+1 < h && j-1 >= 0 && j+1 < w)
-            {
-                for(int tx = 0, x = i -1; x <= i+1; x++,tx++)
-                {
-                    printf("%i\n",tx);
-                    for(int ty=0, y = j-1; y <= j+1; y++,ty++)
-                    {
-                        printf("%f",gxB);
-                        printf("%-----i",image[x][y].rgbtRed);
-                        gxR += aGx[tx][ty] * image[x][y].rgbtRed;
-                        //  gxB += aGx[tx][ty] * image[x][y].rgbtBlue;
-                        //  gxG += aGx[tx][ty] * image[x][y].rgbtGreen;
-                        //  gyR += aGy[tx][ty] * image[x][y].rgbtRed;
-                        //  gyB += aGy[tx][ty] * image[x][y].rgbtBlue;
-                        //  gyG += aGy[tx][ty] * image[x][y].rgbtGreen;
-                        
-                        // totalR += image[x][y].rgbtRed;
-                        // totalB += image[x][y].rgbtBlue;
-                        // totalG += image[x][y].rgbtGreen;
-                        // count++;
-                    }
-        
-                }
-                printf("%f\n",gxB);
-                // image[i][j].rgbtBlue = round(totalB/count);
-                // image[i][j].rgbtRed =  round(totalR/count);
-                // image[i][j].rgbtGreen = round(totalG/count);
-            }
-            
-            
-        }
-    }  
-    return;
+    return 0;
 }
