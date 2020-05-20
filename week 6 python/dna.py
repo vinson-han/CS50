bdb331330ce2e3267f24eab5cda31253a7ea1c3d
vinson-han <44034109+vinson-han@users.noreply.github.com>
2020-05-20 00:50:13 -0700

    Update dna.py

--- week 6 python/dna.py
+++ week 6 python/dna.py
@@ -1,36 +1,30 @@
-import csv
-from sys import argv,exit
-import re
-#1 open csv file and dna sequence, read content into mem
-#2 for each str, compoute the longest run of concescutive in the DNa
-#3compare str count against each row in the csv file
-
-
-#if len(argv) != 3:
-#   print("Usage: python {} data.csv sequence.txt".format(argv[0]))
- #   exit(1)
-
-#filename = argv[1]
-#sequence = argv[2]
-
-filename = "databases/small.csv"
-sequence = "sequences/1.txt"
-d = dict()
-l = list()
-dnaSeq = list()
-dnatext = list()
-with open(filename,newline = '') as csvfile: 
-    reader = csv.DictReader(csvfile)
     for x in (reader):
         l.append(dict(x))
 
 for x in l[0]:
     if(x == 'name'):
         continue
-    dnaSeq.append(x)
+    dnaSeq[x] = 0
+    dnalist.append(x)
 
-print(dnaSeq)
 
+print(dnalist)
 
+templist = list()
+tempd = dict()
 with open(sequence,newline = '') as file: 
     reader = file.read()
+
+#remove after
+rl = len(reader)
+i = len(dnalist[0])
+
+
+
+count = 0
+temp = list()
+tempd = dict()
+for x in range(0,rl):
+    if dnalist[0] == (reader[x:i+x]):
+            count +=1
+    reader[x] = 1
